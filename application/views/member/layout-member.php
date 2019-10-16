eb4f1b843f9a4daec5adcd6ee91c6d1495aadd96
youli <soso830808@gmail.com>
2019-10-16 10:06:59 +0800

    first commit

---
+++ application/views/member/layout-member.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?=$title?></title>
+<script>
+var baseUrl = "<?php echo base_url();?>";
+</script>
+</head>
+<body>
+<?=$freshmanMember_bg?>
+<?=$freshmanMember?>
+</body>
+</html>
